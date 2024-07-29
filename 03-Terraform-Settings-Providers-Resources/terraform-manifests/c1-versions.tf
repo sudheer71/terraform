87b4f656c6cc86394e26388bcdd38d34ae71a825
sudheer <sudheermuthyala71@gmail.com>
2024-07-29 22:18:59 +0530

    --- Auto Git Commit by Sudheer ---

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -1,14 +1,14 @@
 ## Terraform Block 
-terraform               {
-            required_version = "~> 1.9.3"
+terraform {
+  required_version = "~> 1.9.3"
   # If "~> 1.9.3" allows 1.9.3 ,1.9.4, 1.9.5 Deny 1.10.0, 1.10.1,
   # If "~> 1.9"   allows 1.10.xx 1.11.xx
   # NOTE : "The right most it is allowed in this ~>"
 
-                    required_providers {
-    aws                     =               {  #   In side the This required_providers block aws is a argument block with map information in side that  
-      source            = "value"
-      version                           = "value"
+  required_providers {
+    aws = { #   In side the This required_providers block aws is a argument block with map information in side that  
+      source  = "value"
+      version = "value"
 
     }
   }
