ef60492d05d9a152f93cc1657daa1f7598683d38
sudheer <sudheermuthyala71@gmail.com>
2024-07-29 21:54:35 +0530

    --- Auto Git Commit by Sudheer ---

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = "~> 1.9.3"     
   # If "~> 1.9.3" allows 1.9.3 ,1.9.4, 1.9.5 Deny 1.10.0, 1.10.1,
   # If "~> 1.9"   allows 1.10.xx 1.11.xx
-  NOTE The right most it is allowed in this ~>
+  NOTE : "The right most it is allowed in this ~>"
 
                                     
 }
