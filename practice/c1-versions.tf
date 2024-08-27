5cb7c1470facfb1c30d0c5e12cdc279b43805c28
sudheer <sudheermuthyala71@gmail.com>
2024-08-27 18:47:41 +0530

    --- Auto Git Commit by Sudheer ---

--- practice/c1-versions.tf
+++ practice/c1-versions.tf
@@ -7,3 +7,16 @@ terraform {
     }
   }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.64.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
