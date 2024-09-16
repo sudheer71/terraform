cf8dc5f232f28400af6d5bad09c465f27f9145e1
sudheer <sudheermuthyala71@gmail.com>
2024-09-16 10:10:27 +0530

    --- Ms Auto Git Commit ---

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "my-demo" {
     ami = "ami-0b4f379183e5706b9"
     availability_zone = "us-east-1"
     instance_type = "t3.micro"
-    user_data = file("${path.module/}")
+    user_data = file("${path.module/app1}")
 
 }
