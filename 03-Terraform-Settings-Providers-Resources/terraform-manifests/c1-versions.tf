c0658e1f36a35ec2213e15f6618107fe537c210a
sudheer <sudheermuthyala71@gmail.com>
2024-09-16 07:57:16 +0530

    --- Ms Auto Git Commit ---

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -7,9 +7,8 @@ terraform {
 
   required_providers {
     aws = { #   In side the This required_providers block aws is a argument block with map information in side that  
-      source  = "value"
-      version = "value"
-
+      source = "hashicorp/aws"
+      version = ">= 5.0"
     }
   }
 
