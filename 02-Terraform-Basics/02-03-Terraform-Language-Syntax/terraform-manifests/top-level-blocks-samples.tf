8a123501b663dcdaaeaff980be3bf9011247e772
sudheer <sudheermuthyala71@gmail.com>
2024-09-16 07:48:05 +0530

    --- Ms Auto Git Commit ---

--- 02-Terraform-Basics/02-03-Terraform-Language-Syntax/terraform-manifests/top-level-blocks-samples.tf
+++ 02-Terraform-Basics/02-03-Terraform-Language-Syntax/terraform-manifests/top-level-blocks-samples.tf
@@ -2,6 +2,10 @@
 # Block-1: Terraform Settings Block
 terraform {
   required_version = "~> 1.6"
+  # If "~> 1.9.3" allows 1.9.3 ,1.9.4, 1.9.5 Deny 1.10.0, 1.10.1,
+  # If "~> 1.9"   allows 1.10.xx 1.11.xx
+  # NOTE : "The right most it is allowed in this ~>"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
