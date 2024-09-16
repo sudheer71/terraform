141dc051081a4c8fee4e850afb624c68b23594e8
sudheer <sudheermuthyala71@gmail.com>
2024-09-16 07:57:36 +0530

    --- Ms Auto Git Commit ---

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -4,7 +4,7 @@ terraform {
   # If "~> 1.9.3" allows 1.9.3 ,1.9.4, 1.9.5 Deny 1.10.0, 1.10.1,
   # If "~> 1.9"   allows 1.10.xx 1.11.xx
   # NOTE : "The right most it is allowed in this ~>"
-    required_providers {
+  required_providers {
     aws = { #   In side the This required_providers block aws is a argument block with map information in side that  
       source = "hashicorp/aws"
       version = ">= 5.0"
