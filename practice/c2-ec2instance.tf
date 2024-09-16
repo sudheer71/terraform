695bf0a176849717ee91ff350fca3fe551326fb2
sudheer <sudheermuthyala71@gmail.com>
2024-09-16 18:24:34 +0530

    --- Ms Auto Git Commit ---

--- practice/c2-ec2instance.tf
+++ practice/c2-ec2instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mydemo1" {
     ami = "ami-0b4f379183e5706b9"
     availability_zone = "us-east-1"
     instance_type = "t2-micro"
-    user_data = file("${path.module}/app1-in")
+    user_data = file("${path.module}/app1-insta")
 
     tags = {
       name = mydemo1
