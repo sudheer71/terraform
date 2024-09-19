93109bae8842dcd40f3d1ca0bc305549da1c7fda
sudheer <sudheermuthyala71@gmail.com>
2024-09-19 09:24:29 +0530

    --- Ms Auto Git Commit ---

--- 04-Terraform-Variables-and-Datasources/c3-ami-datasource.tf
+++ 04-Terraform-Variables-and-Datasources/c3-ami-datasource.tf
@@ -1,4 +1,21 @@
-data "aws_ami" "my-ami-datasource-amzlinux2" {
+data "aws_ami" "my-" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "^myami-[0-9]{3}"
+  owners           = ["self"]
 
-  
+  filter {
+    name   = "name"
+    values = ["myami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
