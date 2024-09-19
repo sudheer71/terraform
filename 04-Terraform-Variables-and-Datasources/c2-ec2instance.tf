1d89243b0eca64c110f4581c5586570b12e8d2f5
sudheer <sudheermuthyala71@gmail.com>
2024-09-19 09:40:22 +0530

    --- Ms Auto Git Commit ---

--- 04-Terraform-Variables-and-Datasources/c2-ec2instance.tf
+++ 04-Terraform-Variables-and-Datasources/c2-ec2instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mydemo_ec2" {
-    ami = data.aws_ami.my-demo-amzlinux2
+    ami = data.aws_ami.my-demo-amzlinux2.id
     instance_type = "t2.micro"
     user_data = file("${path.module}/app1-install.sh")
 
