21bdfdcac2f9bfe08d746b3eb68dd599c9a0807c
sudheer <sudheermuthyala71@gmail.com>
2024-11-25 09:30:27 +0530

    --- Ms Auto Git Commit ---

--- practice/02-ec2-instance-variables/c2-ec2instance.tf
+++ practice/02-ec2-instance-variables/c2-ec2instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "practice02" {
     ami = ""
     instance_type = var.instance_type
     key_name = var.key_name
-    user_data = file("${path.module}/app1")
+    user_data = file("${path.module}/app1-install.sh")
 
     tags = {
         name = "practic-demo"
