12d5c48669435b71cd24540c3cf1b3829d94fe40
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 19:31:06 +0530

    --- Ms Auto Git Commit ---

--- 04-Terraform-Variables-and-Datasources/c5-ec2instance.tf
+++ 04-Terraform-Variables-and-Datasources/c5-ec2instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my-ec2-demo" {
     key_name = var.instance_keyname
     user_data = file("${path.module}/app1-install.sh")
     instance_type = var.instance_type
-    vpc_security_group_ids = [ aws_security_group.vpc_ssh ]
+    vpc_security_group_ids = [ aws_security_group.vpc_ssh,aws_security_group.vpc_web ]
 
     tags = {
       Name = "My-Demo-Instance"
