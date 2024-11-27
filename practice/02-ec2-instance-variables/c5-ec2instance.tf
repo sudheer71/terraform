fa90f7fd8bbb7b9e6641b457e271a820f9325565
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 09:25:19 +0530

    --- Ms Auto Git Commit ---

--- practice/02-ec2-instance-variables/c5-ec2instance.tf
+++ practice/02-ec2-instance-variables/c5-ec2instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "practice02" {
   instance_type = var.instance_type
   key_name      = var.key_name
   user_data     = file("${path.module}/app1-install.sh")
-  vpc_security_group_ids = [ aws_security_group.practice02_vpc_ssh.id ,]
+  vpc_security_group_ids = [ aws_security_group.practice02_vpc_ssh.id ,aws_security_group.practice02_vpc_web.]
 
   tags = {
     name = "practic-demo"
