4383ccdc2cba98fe6c47fff19ee0637686ede318
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 08:14:42 +0530

    --- Ms Auto Git Commit ---

--- practice/02-ec2-instance-variables/c3-ec2-security-group.tf
+++ practice/02-ec2-instance-variables/c3-ec2-security-group.tf
@@ -1,4 +1,4 @@
-## Security group to allow  SSH
+## Security group to allow SSH Traffice
 resource "aws_security_group" "practice02_vpc_ssh" {
     name = "my_practice_02_ssh"
     description = "Dev_vpc_ssh"
