69f08d9cce8bb875ff5a0614614006a7fc046bfa
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 18:35:40 +0530

    --- Ms Auto Git Commit ---

--- practice/02-ec2-instance-variables/c3-ec2-security-group.tf
+++ practice/02-ec2-instance-variables/c3-ec2-security-group.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "practice02_vpc_web" {
     cidr_blocks = ["0.0.0.0/0"]
 
   }
-  egress = {
+  egress  {
     description = "Allow all port and ip outbound"
     from_port   = 0
     to_port     = 0
