6174b959c917e3009d6e28fceea28ff948ce1e12
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 19:29:35 +0530

    --- Ms Auto Git Commit ---

--- 04-Terraform-Variables-and-Datasources/c5-ec2instance.tf
+++ 04-Terraform-Variables-and-Datasources/c5-ec2instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my-ec2-demo" {
     ami = data.aws_ami.amazonlinux.id
     key_name = var.instance_keyname
-    user_data = file(${path.module}/app1-install.sh)
+    user_data = file("${p}")
     instance_type = ""
     vpc_security_group_ids = ""
 
