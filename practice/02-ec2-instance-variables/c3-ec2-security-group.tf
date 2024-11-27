f2ae876f16580b4bed6dd32a616b7ba43dc065f1
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 08:05:20 +0530

    --- Ms Auto Git Commit ---

--- practice/02-ec2-instance-variables/c3-ec2-security-group.tf
+++ practice/02-ec2-instance-variables/c3-ec2-security-group.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "practice02_vpc_ssh" {
         description = "Allow Port SSH"
         from_port   = 22
         to_port     = 22
-        protocol
+        protocol    = tcp
     }
   
 }
