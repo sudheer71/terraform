6f52a704c927e025bc3fa7e7ef53a334d823ca43
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 18:43:02 +0530

    --- Ms Auto Git Commit ---

--- 04-Terraform-Variables-and-Datasources/c1-versions.tf
+++ 04-Terraform-Variables-and-Datasources/c1-versions.tf
@@ -1,6 +1,11 @@
+
 terraform {
-  required_version = "value"
+  required_version = "~> 1.9.8"
   required_providers {
-    
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
+
