2f017f6acf8dd97edc5a9902ac8e4aeb6b7bfa5c
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 09:26:09 +0530

    --- Ms Auto Git Commit ---

--- practice/02-ec2-instance-variables/c5-ec2instance.tf
+++ practice/02-ec2-instance-variables/c5-ec2instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "practice02" {
   ami           = data.aws_ami.myami_data.id
-  instance_type = var.instance_type
   key_name      = var.key_name
   user_data     = file("${path.module}/app1-install.sh")
+  instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.practice02_vpc_ssh.id ,aws_security_group.practice02_vpc_web.id]
 
   tags = {
