bc5442f3d776e5a12d38c745ab331e2695718d06
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 19:09:42 +0530

    --- Ms Auto Git Commit ---

--- 04-Terraform-Variables-and-Datasources/c3-ec2-security-group.tf
+++ 04-Terraform-Variables-and-Datasources/c3-ec2-security-group.tf
@@ -9,6 +9,7 @@ resource "aws_security_group" "vpc_ssh" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
   egress {
-    description = ""
+    description = "Allow all ip and ports outbound"
+    from_port = 
   }
 }
