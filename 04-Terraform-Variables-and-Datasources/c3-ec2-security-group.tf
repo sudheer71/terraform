cd9d63ba7843e7ac0e9fc23040c4eaf0dc84e244
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 19:08:02 +0530

    --- Ms Auto Git Commit ---

--- 04-Terraform-Variables-and-Datasources/c3-ec2-security-group.tf
+++ 04-Terraform-Variables-and-Datasources/c3-ec2-security-group.tf
@@ -2,6 +2,7 @@ resource "aws_security_group" "vpc_ssh" {
   name = "Allow ssh port"
   description = "Dev_vpc_ssh"
   ingress {
-    description = "allow port ssh "
+    description = "allow port ssh inbond rules"
+    po
   }
 }
