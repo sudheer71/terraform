05fed87b788519dcc918fe724508a3e8213a8918
sudheer <sudheermuthyala71@gmail.com>
2024-11-27 08:59:15 +0530

    --- Ms Auto Git Commit ---

--- practice/02-ec2-instance-variables/c6-ami-datasource.tf
+++ practice/02-ec2-instance-variables/c6-ami-datasource.tf
@@ -1,12 +1,22 @@
-data "aws_ami" "example" {
-  executable_users = ["self"]
-  most_recent      = true
-  name_regex       = "^myami-[0-9]{3}"
-  owners           = ["self"]
 
-  filter {
-    name   = "name"
-    values = ["myami-*"]
-  }
 
-}
+
+
+
+
+
+
+
+
+# data "aws_ami" "example" {
+#   executable_users = ["self"]
+#   most_recent      = true
+#   name_regex       = "^myami-[0-9]{3}"
+#   owners           = ["self"]
+
+#   filter {
+#     name   = "name"
+#     values = ["myami-*"]
+#   }
+
+# }
