2921100aaa2f33e4e216a6284e3e0490b9022818
sudheer <sudheermuthyala71@gmail.com>
2024-11-29 10:14:40 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/c4-ami-datasource.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/c4-ami-datasource.tf
@@ -3,5 +3,26 @@ data "aws_ami" "rkalluru" {
     name_regex = "Centos-8-DevOps-Practice"
     owners = [ "973714476881" ]
 
+    ilter {
+    name = "name"
+    #values = ["al2023-ami-*-x86_64"]
+    values = ["Centos-8-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
   
 }
