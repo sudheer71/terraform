b7411eff3617e6aa643783aec9b2a214ea5923ee
sudheer <sudheermuthyala71@gmail.com>
2024-11-29 10:16:11 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/c5-ec2instance.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/c5-ec2instance.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "myP_05_l_M" {
-    ami = ""
+    ami = var.
     key_name = ""
     user_data = ""
     instance_type = ""
     vpc_security_group_ids = ""
-    
+
     tags = {
       Name = "myP_05_l_M_Demo"
     }
