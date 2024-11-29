fe3dac451cec016422bc049e63abcd7c217f2146
sudheer <sudheermuthyala71@gmail.com>
2024-11-29 10:28:02 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/c5-ec2instance.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/c5-ec2instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "myP_05_l_M" {
     ami = var.aws_ami.rkalluru.id
     key_name = var.key_name
     user_data = file("${path.module}/app1-install.sh")
-    instance_type = ""
+    instance_type = var.instance_type
     vpc_security_group_ids = ""
 
     tags = {
