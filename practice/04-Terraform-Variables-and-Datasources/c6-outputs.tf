f00f8005111268cd8e26286754f57eb8bf99968c
sudheer <sudheermuthyala71@gmail.com>
2024-11-29 09:05:50 +0530

    --- Ms Auto Git Commit ---

--- practice/04-Terraform-Variables-and-Datasources/c6-outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_publicip" {
-  value       = aws_instance.my-ec2-demo.public_ip
-  description = "public_ip"
-}
-
-output "instance_publicdns" {
-  value       = aws_instance.my-ec2-demo.public_dns
-  description = "public_dne"
-}
