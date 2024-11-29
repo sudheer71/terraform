08b474ba1b589e4d454f1c8926218360442d9fe0
sudheer <sudheermuthyala71@gmail.com>
2024-11-29 11:53:45 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/c3-ec2-security-group.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/c3-ec2-security-group.tf
@@ -42,6 +42,7 @@ resource "aws_security_group" "vpc_web" {
   egress {
     description = "Allow all to ports outbound"
     from_port = 0
-    to_port = aws_instance.myP_05_l_M
+    to_port = 0
+    protocol = 
   }
 }
