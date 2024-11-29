f9272dd953b26f8062360d111b97a9baf06f0b29
sudheer <sudheermuthyala71@gmail.com>
2024-11-29 11:53:05 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/c3-ec2-security-group.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/c3-ec2-security-group.tf
@@ -40,6 +40,6 @@ resource "aws_security_group" "vpc_web" {
     cidr_blocks = [ 0.0.0.0/0 ]
   }
   egress {
-    description = "Allow all to ports outboun "
+    description = "Allow all to ports outbound"
   }
 }
