608f3b69743f44118e32a7d32d365b83637fccb8
sudheer <sudheermuthyala71@gmail.com>
2024-11-29 10:15:41 +0530

    --- Ms Auto Git Commit ---

--- 04-Terraform-Variables-and-Datasources/c4-ami-datasource.tf
+++ 04-Terraform-Variables-and-Datasources/c4-ami-datasource.tf
@@ -1,9 +1,9 @@
 data "aws_ami" "amazonlinux" {
   most_recent = true
   # owners     = ["amazon"]
-  owners      = ["973714476881"] # this is DevOps AMi 
+  owners      = ["973714476881"] #DevOps AMi 
   name_regex = "Centos-8-DevOps-Practice"
-  
+
   filter {
     name = "name"
     #values = ["al2023-ami-*-x86_64"]
