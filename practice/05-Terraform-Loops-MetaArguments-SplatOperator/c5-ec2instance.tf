806b0bf45d9d76a878552d072ef49abc681a7439
sudheer <sudheermuthyala71@gmail.com>
2024-12-02 18:45:23 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/c5-ec2instance.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/c5-ec2instance.tf
@@ -5,8 +5,8 @@ resource "aws_instance" "myP_05_l_M" {
   #instance_type          = var.instance_type
   #instance_type = var.instance_type_list[1] # For List
   instance_type = var.instance_type_map["dev"] # For Map
-
   vpc_security_group_ids = [aws_security_group.vpc_ssh.id, aws_security_group.vpc_web.id]
+  
 
   tags = {
     Name = "myP_05_l_M_Demo"
