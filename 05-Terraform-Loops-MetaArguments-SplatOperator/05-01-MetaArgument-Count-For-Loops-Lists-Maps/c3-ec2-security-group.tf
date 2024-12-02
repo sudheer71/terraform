8cbec00b9d33a6839cf487b9a4b740395cfa7577
sudheer <sudheermuthyala71@gmail.com>
2024-12-02 20:18:42 +0530

    --- Ms Auto Git Commit ---

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c3-ec2-security-group.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c3-ec2-security-group.tf
@@ -41,6 +41,6 @@ resource "aws_security_group" "vps_web" {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = [""]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
