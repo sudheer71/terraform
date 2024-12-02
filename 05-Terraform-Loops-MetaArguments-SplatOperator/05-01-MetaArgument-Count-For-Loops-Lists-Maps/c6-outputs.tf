483e6e0f0436e1acb353dba2d098dd9cf465dc8f
sudheer <sudheermuthyala71@gmail.com>
2024-12-02 21:06:50 +0530

    --- Ms Auto Git Commit ---

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c6-outputs.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c6-outputs.tf
@@ -6,10 +6,13 @@
 4. Legacy Splat Operator (latest) - Returns List
 5. Latest Generalized Splat Operator - Returns the List
 */
+
 output "public_ip" {
   description = "public ip"
   value = aws_instance.my_05_l_m.public_ip
 }
+
+
 
 # 1. For Loop with List
 output "public_ip_list" {
