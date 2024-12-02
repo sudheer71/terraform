b48b86fc2a656f0630ae9f984fef9e427e4f74b7
sudheer <sudheermuthyala71@gmail.com>
2024-12-02 21:03:50 +0530

    --- Ms Auto Git Commit ---

---
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c6-outputs.tf
@@ -0,0 +1,9 @@
+# Terraform Output Values
+/* Concepts Covered
+1. For Loop with List
+2. For Loop with Map
+3. For Loop with Map Advanced
+4. Legacy Splat Operator (latest) - Returns List
+5. Latest Generalized Splat Operator - Returns the List
+*/
+
