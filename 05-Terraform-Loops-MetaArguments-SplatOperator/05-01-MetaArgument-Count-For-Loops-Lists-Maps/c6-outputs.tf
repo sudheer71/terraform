ea19aaf6bf8132af9f93b550d5cf9d76ea7505ce
sudheer <sudheermuthyala71@gmail.com>
2024-12-02 21:22:02 +0530

    --- Ms Auto Git Commit ---

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c6-outputs.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c6-outputs.tf
@@ -22,31 +22,31 @@ output "public_dns" {
 
 # 1. For Loop with List
 output "public_ip_list" {
-value = [for instance in aws_instance.my_05_l_m: instance.public_ip]
-description = "For loop with list"
-  
+  value       = [for instance in aws_instance.my_05_l_m : instance.public_ip]
+  description = "For loop with list"
+
 }
 # 2. Output - For Loop with Map
 output "public_ip_map" {
-value = {for instance in aws_aws_instance.my_05_l_m: instance.id => instance.public_ip}
-  
+  value = { for instance in aws_aws_instance.my_05_l_m : instance.id => instance.public_ip }
+
 }
 # 3. Output - For Loop with Map Advanced
 output "public_ip_map_advanced" {
-value = {for c, instance in aws_aws_instance.my_05_l_m: c => instance.public_ip}
-  
+  value = { for c, instance in aws_aws_instance.my_05_l_m : c => instance.public_ip }
+
 }
 
 # 4. Output Legacy Splat Operator (Legacy) - Returns the List
 
 output "legacy_splat_instance_publicdns" {
-    value = aws_instance.my_05_l_m.*.public_dns
-    description = "public DNS using the legacy splat opperators" 
+  value       = aws_instance.my_05_l_m.*.public_dns
+  description = "public DNS using the legacy splat opperators"
 }
 
 # 5. Output Latest Generalized Splat Operator - Returns the List
 output "latest_splat_instance_publicdns" {
-  value = aws_instance.my_05_l_m[*].public_dns
+  value       = aws_instance.my_05_l_m[*].public_dns
   description = "Public DNS using the latest Splate Opperators"
 }
 
