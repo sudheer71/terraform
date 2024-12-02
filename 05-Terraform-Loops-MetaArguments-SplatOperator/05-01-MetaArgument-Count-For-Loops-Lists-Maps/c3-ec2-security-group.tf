d9e09b23e67faff1f3651101a2bbc3a154be394c
sudheer <sudheermuthyala71@gmail.com>
2024-12-02 20:02:12 +0530

    --- Ms Auto Git Commit ---

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c3-ec2-security-group.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c3-ec2-security-group.tf
@@ -2,6 +2,7 @@ resource "aws_security_group" "vpc_ssh" {
     name = "Allow SSH Port"
      description = "Allow port SSH"
   ingress {
-    description = "allow "
+    description = "allow SSH port inbound rules"
+    
   }
 }
