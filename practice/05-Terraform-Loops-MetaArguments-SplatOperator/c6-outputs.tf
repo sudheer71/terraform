2e127d6744490f65715a8c26d0f0d1d499a3c719
sudheer <sudheermuthyala71@gmail.com>
2024-12-02 18:51:53 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/c6-outputs.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/c6-outputs.tf
@@ -26,7 +26,7 @@ output "public_ip" {
   value = [for instance in aws_instance.myP_05_l_M: instance.public_ip]
 }
 
-# Output - For Loop with Map Advanced
+# Output - For Loop with Map
 output "public_ip" {
   description = "for loop with map"
   value = [for instance in aws_instance.myP_05_l_M: instance.id => instance.public_ip]
