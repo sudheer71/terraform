bae5a16e6f62676985286db4955e460e8bf7408c
sudheer <sudheermuthyala71@gmail.com>
2024-12-02 20:55:48 +0530

    --- Ms Auto Git Commit ---

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c5-ec2instance.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/c5-ec2instance.tf
@@ -4,5 +4,6 @@ key_name = var.key_name
 user_data = file("${path.module}/app1.install.sh")
 instance_type = var.instance_type[0]
 vpc_security_group_ids = [ aws_security_group.vpc_ssh.id,aws_security_group.vps_web.id ]
-  
+count = 2
+tags = 
 }
