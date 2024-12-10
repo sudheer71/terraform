e5bc51cdeb17f4d19ab55c160197eb1b0a8960e4
sudheer <sudheermuthyala71@gmail.com>
2024-12-10 19:42:21 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/c3-ec2-security-group.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/c3-ec2-security-group.tf
@@ -40,6 +40,7 @@ resource "aws_security_group" "my_ec2_azd_sg_web" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
   egress {
-    description = "Allow all traffice to "
+    description = "Allow all traffice to outbound traffice"
+    from_port = 
   }
 }
