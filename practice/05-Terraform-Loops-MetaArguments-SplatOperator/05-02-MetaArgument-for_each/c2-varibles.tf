47b87d790b1751f26cbdbb05e0060d292f997b01
sudheer <sudheermuthyala71@gmail.com>
2024-12-10 19:25:29 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/c2-varibles.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/c2-varibles.tf
@@ -19,5 +19,5 @@ variable "instance_type" {
 variable "instance_type_list" {
   description = "ec2 instance"
   type = list(string)
-  default = [ "t2.micro","t3.small", ]
+  default = [ "t2.micro","t3.small","t3" ]
 }
