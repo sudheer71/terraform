56d3717f37310fbb2db846b0f0c4e8d9107a4baf
sudheer <sudheermuthyala71@gmail.com>
2024-12-10 19:46:52 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/c5-ec2instance.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/c5-ec2instance.tf
@@ -1,7 +1,7 @@
 ## meta_aurgment_for_each_az_d
 resource "aws_instance" "my_ec2_azd" {
   ami = data.aws_ami.my_ec2_azd_ami.id
-  key_name = 
+  key_name = var.key_name
   instance_type = ""
   vpc_security_group_ids = ""
   availability_zone = ""
