b064388c3e9d54ef7bb760d5b3e3720638b27f46
sudheer <sudheermuthyala71@gmail.com>
2024-12-10 19:45:52 +0530

    --- Ms Auto Git Commit ---

--- practice/05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/c4-ami-datasource.tf
+++ practice/05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/c4-ami-datasource.tf
@@ -2,7 +2,7 @@ data "aws_ami" "my_ec2_azd_ami" {
     most_recent = true
     name_regex = "Centos-8-DevOps-Practice"
     owners      = ["973714476881"] #DevOps AMi
-  #owners     = ["amazon"]
+    #owners     = ["amazon"]
 
   filter {
     name = "name"
